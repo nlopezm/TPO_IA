f022bff52f255c98a8a7f25b6db2f844f21567e5
nlopezm <nlopezmarini@gmail.com>
2018-10-26 10:47:51 -0300

    Enable cors

--- index.php
+++ index.php
@@ -17,6 +17,11 @@ session_start();
 $settings = require __DIR__ . '/app/settings.php';
 $app = new \Slim\App($settings);
 
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response->withHeader('Access-Control-Allow-Origin', '*');
+});
+
 // Set up dependencies
 require __DIR__ . '/app/dependencies.php';
 
